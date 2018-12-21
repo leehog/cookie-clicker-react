fea5a978a5f683c699cb9246c9779c58efe78126
leehog <leeeehoog@gmail.com>
2018-12-21 16:55:43 +0100

    Removed console logs

--- src/actions/cookie.action.js
+++ src/actions/cookie.action.js
@@ -1,7 +1,6 @@
 import { ADD_COOKIE } from '../types'
 
 export const addCookies = (cookies) => {
-    console.log(cookies)
     return {
         type: ADD_COOKIE,
         payload: cookies    
--- src/reducers/cookie.reducer.js
+++ src/reducers/cookie.reducer.js
@@ -6,7 +6,6 @@ const INITIAL_STATE = {
 }
 
 export default function(state = INITIAL_STATE, action) {
-    console.log(state.cookies)
     switch(action.type){
         case ADD_COOKIE: {
             return {
